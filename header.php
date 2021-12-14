5db8c2428f13264f4844945242575d83f4eecb82
Andrew Nyland <andrewlnyland@gmail.com>
2021-12-14 12:10:25 -0700

    Cleaning up webserver moved

--- header.php
+++
@@ -1,7 +0,0 @@
-<nav>
-	<h1>ElectroMyogram Controller</h1>
-	<div id="metadata">
-		<div><?php echo $_SESSION['temp'];?></div>
-		<div><small>pid:</small> <?php echo $_SESSION['pyPID'];?></div>
-	</div>
-</nav>
